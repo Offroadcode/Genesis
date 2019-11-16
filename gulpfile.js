4f509eb3f1a38167c52470411b8602919542ec54
James Young <james@welcomebrand.co.uk>
2019-11-16 20:03:31 +0000

    Updated Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,146 +1,70 @@
-'use strict';
- 
-var gulp = require('gulp');
-var gulpif = require('gulp-if');
-//var runSequence = require('run-sequence');
-var del = require('del');
-var sass = require('gulp-sass');
-var jshint = require('gulp-jshint');
-var uglify = require('gulp-uglify');
-var concat = require('gulp-concat');
-var rename = require('gulp-rename');
-var watchify = require('watchify');
-var sourcemaps = require('gulp-sourcemaps');
-var gutil = require('gulp-util');
-var runSequence = require('run-sequence').use(gulp);
-var postcss = require('gulp-postcss');
-var svgstore = require('gulp-svgstore');
-var svgmin = require('gulp-svgmin');
-var imagemin = require('gulp-imagemin');
-var compress = require('compression');
-var cache = require('gulp-cached');
-
-/**
- * Build config
- */
-var config = {
-    browserslist: [
-        'Explorer >= 11',
-        'Edge >= 38',
-        'Chrome >= 61',
-        'Safari >= 10',
-        'Firefox >= 55',
-        'iOS >= 10',
-        'Android >= 4.4'
-    ]
-};
-
-// Compiles Sass to CSS
-gulp.task('sass', function () {
-    var plugins = [
-        require('autoprefixer')({
-            browsers: config.browserslist
-        }),
-        require('postcss-inline-svg'), // inlines SVG and customizes styles https://github.com/TrySound/postcss-inline-svg
-        require('postcss-pxtorem'), // Fenerates rem units from pixel units https://github.com/cuth/postcss-pxtorem
-        require('postcss-object-fit-images')
-    ];
-
-    return gulp.src('Build/assets/scss/**/*.scss')
-        .pipe(sourcemaps.init())
-        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
-        .pipe(postcss(plugins))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('Build/assets/css/'));
-});
-
-// Minifies and optimise images
-gulp.task('imagemin', function () {
-    return gulp.src('Build/assets/img-raw/*')
-        .pipe(imagemin([
-            imagemin.gifsicle(), 
-            imagemin.jpegtran(), 
-            imagemin.optipng(),
-            imagemin.svgo({
-                plugins: [
-                    {removeViewBox: true},
-                    {cleanupIDs: false}
-                ]
-            })
-        ]))
-        .on('error', gutil.log)
-        .pipe(gulp.dest('Build/assets/img'));
-});
-
-// SVG symbol generation
-gulp.task('svgstore', function () {
-    return gulp
-        .src('Build/assets/img-raw/*.svg')
-        .pipe(svgmin(function (file) {
-            return {
-                plugins: [{
-                    cleanupIDs: {
-                        minify: true
-                    }
-                }]
-            }
-        }))
-        .pipe(svgstore())
-        .on('error', gutil.log)  
-        .pipe(rename('icon-sprite.svg'))
-        .pipe(gulp.dest('Build/assets/img'));
-});
+// Initialize modules
+// Importing specific gulp API functions lets us write them below as series() instead of gulp.series()
+const { src, dest, watch, series, parallel } = require('gulp');
+// Importing all the Gulp-related packages we want to use
+const sourcemaps = require('gulp-sourcemaps');
+const sass = require('gulp-sass');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+var replace = require('gulp-replace');
 
 
-// Concatenate & Minify JS - https://travismaynard.com/writing/getting-started-with-gulp
-gulp.task('scripts', function() {
-    return gulp.src(['Build/assets/js/app/*.js', 'Build/assets/js/libs/*.js'])
-        .pipe(concat('production.js'))
-        .pipe(gulp.dest('Build/assets/js/build'))
-        .pipe(rename('production.min.js'))
+// File paths
+const files = { 
+    scssPath: 'Build/assets/scss/**/*.scss',
+    jsPath: 'Build/assets/js/**/*.js'
+}
+
+// Sass task: compiles the style.scss file into style.css
+function scssTask(){    
+    return src(files.scssPath)
+        .pipe(sourcemaps.init()) // initialize sourcemaps first
+        .pipe(sass()) // compile SCSS to CSS
+        .pipe(postcss([ autoprefixer(), cssnano() ])) // PostCSS plugins
+        .pipe(sourcemaps.write('.')) // write sourcemaps file in current directory
+        .pipe(dest('Build/assets/production/css')
+    ); // put final CSS in dist folder
+}
+
+// JS task: concatenates and uglifies JS files to script.js
+function jsTask(){
+    return src([
+        files.jsPath
+        //,'!' + 'includes/js/jquery.min.js', // to exclude any specific files
+        ])
+        .pipe(concat('all.js'))
         .pipe(uglify())
-        .pipe(gulp.dest('Build/assets/js/build'));
-});
- 
-// Watch Files For Changes - https://travismaynard.com/writing/getting-started-with-gulp
-gulp.task('watch', function() {
-    gulp.watch('Build/assets/js/libs/*.js', gulp.series('scripts'));
-    gulp.watch('Build/assets/js/app/*.js', gulp.series('scripts'));
-    gulp.watch('Build/assets/scss/**/*.scss', gulp.series('sass'));
-});
-
-
-// Critical CSS extraction 
-gulp.task('critical-css', function(cb) {
-    
-    critical.generate({
-        base: 'Build/',
-        src: 'http://genesis.local/',
-        dest: 'assets/css/critical.css',
-        width: 320,
-        height: 480,
-        inline: false
-    });
-
-});
-
-
-
-
-
-// Default task
-gulp.task('default', function( callback ){
-    runSequence(
-        ['sass', 'svgstore', 'imagemin', 'scripts', 'fileinclude', 'watch'], 
-        callback
+        .pipe(dest('Build/assets/production/js')
     );
-});
+}
 
+// Cachebust
+var cbString = new Date().getTime();
+function cacheBustTask(){
+    return src(['Build/index.html'])
+        .pipe(replace(/cb=\d+/g, 'cb=' + cbString))
+        .pipe(dest('.'));
+}
 
-// Generate Critical CSS only
-gulp.task('critical', function( callback ){
-    runSequence(
-        ['critical-css'], 
-        callback
-    );
-});
+// Watch task: watch SCSS and JS files for changes
+// If any change, run scss and js tasks simultaneously
+function watchTask(){
+    watch([files.scssPath, files.jsPath], 
+        series(
+            parallel(scssTask, jsTask),
+            cacheBustTask
+        )
+    );    
+}
+
+// Export the default Gulp task so it can be run
+// Runs the scss and js tasks simultaneously
+// then runs cacheBust, then watch task
+exports.default = series(
+    parallel(scssTask, jsTask), 
+    cacheBustTask,
+    watchTask
+);
